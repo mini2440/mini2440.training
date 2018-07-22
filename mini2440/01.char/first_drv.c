3d5d145be2b3ba997394d5bd401feee5f31f675a
mz8023yt <mz8023yt@163.com>
2018-07-22 20:04:08 +0800

    feature: realize the second_drv

--- mini2440/01.char/first_drv.c
+++ mini2440/01.char/first_drv.c
@@ -4,7 +4,7 @@
 #include <linux/cdev.h>
 #include <linux/device.h>
 #include <asm/uaccess.h>        /* copy_to_user ... */
-#include <linux/ioctl.h>        
+#include <linux/ioctl.h>        /* ioctl ... */
 
 /* simple log */
 #define log(fmt, arg...)\
@@ -50,109 +50,78 @@ int first_ioctl(struct file *filp, unsigned int cmd, unsigned long arg)
 
 static int first_open(struct inode *indoe, struct file *filp)
 {
-        log("first open ok");
+        log("first_drv open ok");
         return 0;
 }
 
 static int first_release(struct inode *indoe, struct file *filp)
 {
-        log("first release ok");
+        log("first_drv release ok");
         return 0;
 }
 
 static ssize_t first_read(struct file *filp, char __user *buff, size_t cnt, loff_t *loff)
 {
-        int tmp = 128;
-        copy_to_user(buff, &tmp, 4);
-        log("first read ok");
+        log("first_drv read ok");
         return 0;
 }
 
 static ssize_t first_write(struct file *filp, const char __user *buff, size_t cnt, loff_t *loff)
 {
-        int tmp = 0;
-        copy_from_user(&tmp, buff, 4);
-        log("tmp = %d", tmp);
-        log("first write ok");
+        log("first_drv write ok");
         return 0;
 }
 
 static struct file_operations first_drv_fops =
 {
-        .open    = first_open,
-        .release = first_release,
-        .read    = first_read,
-        .write   = first_write,
+        .open           = first_open,
+        .release        = first_release,
+        .read           = first_read,
+        .write          = first_write,
         .unlocked_ioctl = first_ioctl,
 };
 
-static int first_drv_major;
-static int first_drv_minor;
-struct cdev *first_drv;                                 /* cdev 数据结构 */
-static dev_t first_drv_devno;                           /* 设备编号 */
-static struct class *first_drv_class;
+struct cdev *first_drv;                 /* cdev 数据结构 */
+static dev_t first_drv_devno;           /* 设备编号 */
+static struct class *first_drv_class;   /* 用于自动创建设备的类 */
 
 static int __init mod_init(void)
 {
-        int ret;
-
-        /* 
-         * 从系统获取主设备号，这里指定的驱动名称将会在 /proc/device 下面显示
-         */
-        ret = alloc_chrdev_region(&first_drv_devno, first_drv_minor, 1, "first_drv_name");
-        if (ret < 0)
-        {
-                log("cannot get major %d \n", first_drv_major);
-                return -1;
-        }
-
-        first_drv_major = MAJOR(first_drv_devno);
+        /* 从系统获取主设备号，这里指定的驱动名称将会在 /proc/device 下面显示 */
+        alloc_chrdev_region(&first_drv_devno, 0, 1, "first_drv_name");
 
         /* 分配 led 结构 */
         first_drv = cdev_alloc();
-        if (first_drv != NULL)
-        {
-                /* 初始化字符设备结构，并绑定操作函数 */
-                cdev_init(first_drv, &first_drv_fops); 
-                first_drv->owner = THIS_MODULE;
+
+        /* 初始化字符设备结构，并绑定操作函数 */
+        cdev_init(first_drv, &first_drv_fops); 
+        first_drv->owner = THIS_MODULE;
                 
-                /* 将字符设备添加到系统中 */
-                if (cdev_add(first_drv, first_drv_devno, 1) != 0)
-                {
-                        log("add cdev error!\n");
-                        goto error;
-                }
-        }
-        else
-        {
-                log("cdev_alloc error!\n");
-                return -1;
-        }
+        /* 将字符设备添加到系统中 */
+        cdev_add(first_drv, first_drv_devno, 1);
 
         /* 在 /sys/class 目录下创建一个 first_drv_class */
         first_drv_class = class_create(THIS_MODULE, "first_drv_class");
-        if (IS_ERR(first_drv_class)) {
-                log("create class error\n");
-                return -1;
-        }
 
         /* 在 /sys/class/first_drv_class 目录下创建一个 first_drv_device */
         device_create(first_drv_class, NULL, first_drv_devno, "first_drv_device");
 
         log("mod_init ok");
         return 0;
-
-error:
-        unregister_chrdev_region(first_drv_devno, 1);
-        return ret;
 }
 
 static void __exit mod_exit(void)
 {
-        cdev_del(first_drv);                                    /* 移除字符设备 */
-        unregister_chrdev_region(first_drv_devno, 1);           /* 释放设备号 */
+        /* 从系统中移除字符设备 */
+        cdev_del(first_drv);
+
+        /* 释放申请的设备号 */
+        unregister_chrdev_region(first_drv_devno, 1);
+
+        /* 销毁之前创建的 device 和 class*/
         device_destroy(first_drv_class, first_drv_devno);
         class_destroy(first_drv_class);
+
         log("mod_exit ok");
 }
 
---
+++ mini2440/02.memory/second_drv.c
@@ -0,0 +1,203 @@
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/fs.h>
+#include <linux/cdev.h>
+#include <linux/device.h>
+#include <asm/uaccess.h>        /* copy_to_user ... */
+#include <linux/ioctl.h>        /* ioctl ... */
+#include <linux/slab.h>         /* kmalloc ... */
+#include <linux/proc_fs.h>      /* procfs ... */
+
+/* simple log */
+#define log(fmt, arg...)\
+        printk(KERN_INFO "[Paul][%s][%d] "fmt"\n", __func__, __LINE__, ##arg);
+
+#define MEM_SIZE (4*1024*1024)
+
+struct share {
+        unsigned char mem[MEM_SIZE];  // share mem
+};
+
+struct share *sh;
+
+static int second_open(struct inode *indoe, struct file *filp)
+{
+        log("second_drv open ok");
+        return 0;
+}
+
+static int second_release(struct inode *indoe, struct file *filp)
+{
+        log("second_drv release ok");
+        return 0;
+}
+
+static struct file_operations second_drv_fops =
+{
+        .open           = second_open,
+        .release        = second_release,
+};
+
+static struct proc_dir_entry* proc_dir = NULL;
+static struct proc_dir_entry* proc_node = NULL;
+
+static loff_t second_drv_proc_llseek(struct file *filp, loff_t offset, int whence)
+{
+        loff_t new_pos = 0;
+        switch(whence)
+        {
+                case 0: /* SEEK_SET */
+                        new_pos = offset;
+                        break;
+ 
+                case 1: /* SEEK_CUR */
+                        new_pos = filp->f_pos + offset;
+                        break;
+ 
+                case 2: /* SEEK_END */
+                        new_pos = MEM_SIZE -1 + offset;
+                        break;
+ 
+                default: /* can't happen */
+                        return -EINVAL;
+        }
+
+        if ((new_pos < 0) || (new_pos > MEM_SIZE))
+                return -EINVAL; 
+        filp->f_pos = new_pos;
+        return new_pos;
+}
+
+static ssize_t second_drv_proc_read(struct file *filp, char __user *buff, size_t cnt, loff_t *loff)
+{
+        unsigned long p = *loff;
+        unsigned int count = cnt;
+        int ret = 0;
+        if(p >= MEM_SIZE)
+                return 0;
+        if(count > MEM_SIZE - p)
+                count = MEM_SIZE - p;
+
+        if(copy_to_user(buff, sh->mem + p, count))
+                ret = -EFAULT;
+        else
+        {
+                *loff += count;
+                ret = count;
+                log("read size = %d", count);
+        }
+
+        log("second_drv proc_read write ok");
+        return ret;
+}
+
+static ssize_t second_drv_proc_write(struct file *filp, const char __user *buff, size_t cnt, loff_t *loff)
+{
+        unsigned long p = *loff;
+        unsigned int count = cnt;
+        int ret = 0;
+
+        if(p >= MEM_SIZE)
+                return 0;
+        if(count > MEM_SIZE - p)
+                count = MEM_SIZE - p;
+        
+        if(copy_from_user(sh->mem + p, buff, count))
+                ret = -EFAULT;
+        else
+        {
+                *loff += count;
+                ret = count;
+                log("write size = %d", count);
+        }
+
+        log("second_drv proc_write write ok");
+        return ret;
+}
+
+
+static const struct file_operations second_drv_proc_fops = 
+{
+        .owner  = THIS_MODULE,
+        .llseek = second_drv_proc_llseek,
+        .read   = second_drv_proc_read,
+        .write  = second_drv_proc_write,
+};
+
+struct cdev *second_drv;                 /* cdev 数据结构 */
+static dev_t second_drv_devno;           /* 设备编号 */
+static struct class *second_drv_class;   /* 用于自动创建设备的类 */
+
+static int __init mod_init(void)
+{
+        /* 从系统获取主设备号，这里指定的驱动名称将会在 /proc/device 下面显示 */
+        alloc_chrdev_region(&second_drv_devno, 0, 1, "second_drv_name");
+
+        /* 分配 led 结构 */
+        second_drv = cdev_alloc();
+
+        /* 初始化字符设备结构，并绑定操作函数 */
+        cdev_init(second_drv, &second_drv_fops); 
+        second_drv->owner = THIS_MODULE;
+                
+        /* 将字符设备添加到系统中 */
+        cdev_add(second_drv, second_drv_devno, 1);
+
+        /* 在 /sys/class 目录下创建一个 second_drv_class */
+        second_drv_class = class_create(THIS_MODULE, "second_drv_class");
+
+        /* 在 /sys/class/second_drv_class 目录下创建一个 second_drv_device */
+        device_create(second_drv_class, NULL, second_drv_devno, "second_drv_device");
+
+        /* 申请 4M 内存，申请的内存比较大，比较容易出错，务必要检查函数返回值 */
+        sh = (unsigned char *)kzalloc(MEM_SIZE, GFP_KERNEL);
+        if (sh == NULL) {
+                kfree(sh);
+                return -ENOBUFS;
+        }
+
+        /* 在 /proc 目录下创建 share 子目录 */
+        proc_dir = proc_mkdir("share", NULL);
+        if(!proc_dir)
+        {
+                remove_proc_entry("share", NULL);
+                return -ENOMEM;
+        }
+
+        /* 在 /proc/share 目录下创建 mem 节点 */
+        proc_node = create_proc_entry("mem", 0666, proc_dir);
+        proc_node->proc_fops = &second_drv_proc_fops;
+        if(!proc_node)
+        {
+                remove_proc_entry("mem", proc_dir);
+                return -ENOMEM;
+        }
+
+        log("mod_init ok");
+        return 0;
+}
+
+static void __exit mod_exit(void)
+{
+        /* 从系统中移除字符设备 */
+        cdev_del(second_drv);
+
+        /* 释放申请的设备号 */
+        unregister_chrdev_region(second_drv_devno, 1);
+
+        /* 销毁之前创建的 device 和 class */
+        device_destroy(second_drv_class, second_drv_devno);
+        class_destroy(second_drv_class);
+
+        /* 移除之前创建的 proc 目录和节点 */
+        remove_proc_entry("mem", proc_dir);
+        remove_proc_entry("share", NULL);
+
+        log("mod_exit ok");
+}
+
+module_init(mod_init);
+module_exit(mod_exit);
+
+MODULE_LICENSE("GPL");
+
